69ac2a96386c33b5e2d9b9ecc35f0570e8de5509
kadyrkaragishiev@gmail.com <kadyrkaragishiev@gmail.com>
2019-03-29 18:10:58 +0300

    commit

---
+++ Assets/MyAssets/Scripts/InstanceDelievery.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceDelievery : MonoBehaviour
+{
+    public GameObject gObj;
+
+
+    private void Start()
+    {
+        StartCoroutine(FindClient());
+    }
+    private IEnumerator FindClient()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(4f);
+            try
+            {
+                SomeFunc();
+            }
+            catch (IndexOutOfRangeException e){
+                Debug.Log("error");
+            }
+        }
+    }
+
+    void SomeFunc()
+    {
+        var Tables = GameObject.FindGameObjectsWithTag("Table");
+        int k = UnityEngine.Random.Range(0, Tables.Length);
+        CreateAClient(Tables[k].transform, Tables[k]);
+    }
+
+    void CreateAClient(Transform table,GameObject g_table)
+    {
+        foreach (Transform eachChild in table)
+        {
+            if (eachChild.name == "InstanceForButton")
+            {
+                var instance_pos = table.Find("InstanceForButton");
+                Instantiate(gObj, instance_pos.position, Quaternion.identity);
+                table.gameObject.tag = "NotUse";
+                Debug.Log(g_table.tag);
+            }
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/TableScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScript : MonoBehaviour
+{
+    bool zakaz = true;
+    private void Start()
+    {
+        Check();
+    }
+    public void OnTableClick()
+    {
+        Check();
+        if (zakaz)
+        {
+            this.gameObject.tag = "Table";
+        }
+        Debug.Log("CLICK");
+    }
+    void Check()
+    {
+        if (this.gameObject.tag == "Table")
+        {
+            Debug.Log("Zakaz");
+            zakaz = false;
+        }
+        else
+        {
+            zakaz = true;
+        }
+    }
+}
